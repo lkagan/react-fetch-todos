f10315b6253cdf9f2590afc265e11e8c7ce4600c
Larry Kagan <larry@superiocity.com>
2022-06-16 10:35:35 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [todos, setTodos] = useState();
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+        .then(response => response.json())
+        .then(json => setTodos(json))
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul style={{textAlign: 'left'}}>
+        { todos?.map(todo => <li key={todo.id}>{todo.title}</li>)
+        }
+      </ul>
     </div>
   );
 }
